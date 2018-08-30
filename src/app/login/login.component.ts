8bfd58108f447282b88f30b886b64ef0c46f1323
prabakargr <prabakar1993@gmail.com>
2018-08-30 17:28:16 +0530

    authendication created

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import {DataService} from '../data.service';
 import { User } from '../user/user';
+import {AuthService} from '../service/auth.service';
 
 
 @Component({
@@ -12,20 +13,53 @@ import { User } from '../user/user';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email = '';
+  password = '';
+  errorMessage = '';
+  error: {name: string, message: string} = {name: '', message: ''};
 newLogin:any
-  constructor( private router : Router,private dataservice:DataService) { }
+  constructor( private router : Router,
+               private dataservice:DataService,
+               public authService: AuthService) { }
   ngOnInit() {
-    this.dataservice.getnewLogin().subscribe(res=>{
-      this.newLogin=res;
-      for(var i=0;i<this.newLogin.length;i++){
-       
+    this.dataservice.getnewLogin().subscribe(res => {
+      this.newLogin = res;
+      for (var i = 0; i < this.newLogin.length; i++) {
+
         console.log(res);
+      }
+
+    })
+  }
+onLogin(): void {
+  if (this.validateForm(this.email, this.password)) {
+    this.authService.loginWithEmail(this.email, this.password)
+      .then(() => this.router.navigate(['/landpage']))
+      .catch(_error => {
+        this.error = _error
+        this.router.navigate(['/login'])
+      })
+  }
+}
+validateForm(email: string, password: string): boolean {
+  if (email.length === 0) {
+    this.errorMessage = 'Please enter Email!'
+    return false
   }
 
-})
+  if (password.length === 0) {
+    this.errorMessage = 'Please enter Password!'
+    return false
   }
-onSubmit1(value){
-  this.router.navigate(['/landpage/home']);
+
+  if (password.length < 6) {
+    this.errorMessage = 'Password should be at least 6 characters!'
+    return false
+  }
+
+  this.errorMessage = ''
+
+  return true
 }
   
 } 
